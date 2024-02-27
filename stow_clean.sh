6ebb1d180ee6c7fb53d66a9dd87275eb1cbe85b1
aimawari <aimawari.ai@gmail.com>
2024-02-27 21:33:41 +0700

    Fix wrong alacritty path

--- stow_clean.sh
+++ stow_clean.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # From: https://github.com/ThePrimeagen/.dotfiles/blob/master/clean-env
-export STOW_FOLDERS="alacritty,bin,brew,nvim,tmux,zsh"
+export STOW_FOLDERS="alacritty,bin,nvim,tmux,zsh"
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
     echo "› Removing $folder"
