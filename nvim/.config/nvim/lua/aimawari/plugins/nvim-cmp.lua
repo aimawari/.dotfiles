fa462a59c44fb47b5eaa05344ed594da2ead1d3c
aimawari <aimawari.ai@gmail.com>
2025-06-17 16:50:11 +0700

    Update dotfiles

--- nvim/.config/nvim/lua/aimawari/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/aimawari/plugins/nvim-cmp.lua
@@ -7,7 +7,6 @@ return {
         "saadparwaiz1/cmp_luasnip",     -- for autocompletion
         "rafamadriz/friendly-snippets", -- useful snippets
         "onsails/lspkind.nvim",         -- vs-code like pictograms
-        "vurentjie/cmp-gl"              -- openGL cmp
     },
     config = function()
         local cmp = require("cmp")
